13b46cf3ccfab7dc4a7d29b68f6951569e44cd29
btahir89 <bilal.tahir89@gmail.com>
2025-03-05 10:43:00 -0800

    more crap

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = [
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/ban-ts-comment': 'off',
+      'prefer-rest-params': 'off',
     },
   },
 ]
